49da060a8ac8e563ff78290a89af2ff96ff67033
Jithin Mohanan <jitsonfire@gmail.com>
2016-09-17 14:08:59 +0530

    Updated code

--- UI/app/Scripts/controller/SetPasswordController.js
+++ UI/app/Scripts/controller/SetPasswordController.js
@@ -1,10 +1,39 @@
 app.controller('SetPasswordController', function($scope, $http, $uibModal,$location,$window) {
 
-    $scope.host = $location.host();
+
     $scope.setPassword = {};
-    $scope.setPassword.passwordResetCode =  $location.search().passwordResetCode;
-    $scope.passwordToken = $location.search().passwordResetCode;
-    $scope.setPassword = function() {
+    $scope.setPassword.activiationId =  $location.search().passwordResetCode;
+    $scope.host = $location.host();
+    $scope.setPasswordForUser = function() {
+
+      var modalRequest = {};
+      modalRequest.method = 'post';
+      modalRequest.url = 'http://' + $scope.host + '/StolenVehicle/setPassword';
+      modalRequest.payLoad = $scope.setPassword;
+      modalRequest.entityAttribute = 'setPassword';
+      modalRequest.successMessage = 'Password has been successfully udpated';
+      modalRequest.modalTime = 2000;
+      modalRequest.modalInstance = $uibModal.open({
+          animation: true,
+          templateUrl: 'dialog/loader.html',
+          controller: 'ModalController',
+          size: 'md',
+          resolve: {
+              request: modalRequest
+          }
+
+      });
+
+      modalRequest.modalInstance.result.then(function(result) {
+
+            $window.location='/#/login';
+
+
+      }, function() {
+
+
+
+      });
 
     };
 
