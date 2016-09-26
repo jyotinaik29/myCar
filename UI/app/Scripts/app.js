4e828e1d9f027325ab31f9c75a08a2f5a0179482
Jithin Mohanan <jitsonfire@gmail.com>
2016-09-26 08:44:24 +0530

    updated code

--- UI/app/Scripts/app.js
+++ UI/app/Scripts/app.js
@@ -138,3 +138,25 @@ app.constant('uiDatetimePickerConfig', {
         saveAs: false,
         readAs: false,
     });
+
+
+    var compareTo = function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+};
+
+app.directive("compareTo", compareTo);
